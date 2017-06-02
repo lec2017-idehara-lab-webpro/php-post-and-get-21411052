2e036f16cfeded9e8a664a2423e50b6ccb2af6ca
21411052 <21411052ho@tama.ac.jp>
2017-06-02 14:30:48 +0900

    update

--- hello.php
+++ hello.php
@@ -2,15 +2,26 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>hello</title>
   </head>
   <body>
 
-<?php
+    <?php
+    	if( !isset($_GET['name'],$_GET['id']) ||
+    		strlen($_GET['name']) == 0 ||
+    		strlen($_GET['id']) == 0
+        )
+    	{
+    		echo 'Missing Data';
+    	}
+    	else
+    	{
+    		$id = ($_GET['id']) ;
+        $name = ($_GET['name']);
+      }
+      echo "$id の $name さん、こんにちは";
 
-var_dump($_GET);
-
-?>
+      ?>
 
 
   </body>
--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>index</title>
   </head>
   <body>
 
-    <form action='hello.php' method='get'>
-      <input type='text' name='id'>
-      <input type='text' name='name'>
+    <form action='hello.php' method='GET'>
+      <table>
+      <tr><th>学籍番号</th><td><input type='text' name='id'></td></tr>
+      <tr><th>氏名</th><td><input type='text' name='name'></td></tr>
+      </table>
       <input type='submit'>
     </form>
 
